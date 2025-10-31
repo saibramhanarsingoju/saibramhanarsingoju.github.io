7d45c6eadff37f7e04783b711df4198213c79a69
saibramha narsingoju <saibramha464@gmail.com>
2025-10-31 17:02:18 -0500

    Initial portfolio design with projects, skills, and resume

--- scripts.js
+++ scripts.js
@@ -1,2 +1,9 @@
-// script.js
-console.log("Saibramha Narsingoju Portfolio Loaded");
+// Smooth scrolling for navigation links (if you add nav later)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
